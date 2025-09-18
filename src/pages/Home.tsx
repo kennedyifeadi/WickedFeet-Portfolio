1d418dd11dcfdb1ecfb16a571167f5f415f3306c
Kennedy Ifeadi <kennedyifeadi@gmail.com>
2025-09-19 00:26:42 +0100

    fix: sizes of container

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,8 +25,8 @@ const Home = () => {
     requestAnimationFrame(raf)
   }, [])
   return (
-    <div className="relative h-[500vh] bg-[#1A1A1A]">
-      <div ref={container} className="relative md:h-[200vh] h-[250vh]">
+    <div className="relative h-[500dvh] bg-[#1A1A1A]">
+      <div ref={container} className="relative md:h-[200dvh] h-[250dvh]">
         <Landing scrollYProgress={scrollYProgress} />
         <AboutMe />
       </div>

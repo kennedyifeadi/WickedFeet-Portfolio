9ff66966c650242196933667659490d6487d54e7
Kennedy Ifeadi <kennedyifeadi@gmail.com>
2025-09-19 00:19:30 +0100

    fix: changed lowercase to uppercase

--- src/components/homeComp/AboutMe.tsx
+++ src/components/homeComp/AboutMe.tsx
@@ -2,7 +2,7 @@ import { useState, useRef } from "react";
 import { FaPlay } from "react-icons/fa6";
 
 
-import Dancervid from "../../assets/dancerVid.mp4";
+import Dancervid from "../../assets/Dancervid.mp4";
 
 export const AboutMe = () => {
   const [isPlaying, setIsPlaying] = useState(false);
